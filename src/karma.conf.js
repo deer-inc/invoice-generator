b20947ea88e7d8571a41fdeb493db48ff6d5873c
Daichi Ninomiya <daichi.ninomiya@deer.co.jp>
2019-03-13 22:36:37 +0900

    test(travis): add chrome headless config

--- src/karma.conf.js
+++ src/karma.conf.js
@@ -26,6 +26,10 @@ module.exports = function (config) {
     logLevel: config.LOG_INFO,
     autoWatch: true,
     browsers: ['Chrome'],
+    ChromeHeadlessCI: {
+      base: 'ChromeHeadless',
+      flags: ['--no-sandbox']
+    },
     singleRun: false,
     restartOnFileChange: true
   });
